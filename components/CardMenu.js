03f7e07d77038cf40b4fcab9c8b6f251fa3cc3ef
Anthony Yan <anthonyyan@Anthonys-MacBook-Pro.local>
2021-06-26 18:07:30 -0500

    added Card components

--- components/CardMenu.js
+++ components/CardMenu.js
@@ -1,3 +1,4 @@
+
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import colors from '../config/colors.js';
@@ -5,39 +6,62 @@ import Card from './Card.js';
 
 const CardMenu = () => {
     const [cardList, setCardList] = useState([])
+    //const [cardPressed, setCardPressed] = useState('')
+    
     useEffect(()=>{
         setCardList([{
             name: "Containers",
-            icon: ""
+            icon: "box-open"
         },
         {
             name: "Utensils",
-            icon: ""
+            icon: "utensils"
         },
         {  
             name: "Bottles",
-            icon: ""
+            icon: "wine-bottle"
         },
         {
             name: "Cans",
-            icon: ""
+            icon: "prescription-bottle"
+        },
+        {
+            name: "Bags",
+            icon: "shopping-bag"
         },
         {
             name: "Electronics",
-            icon: ""
+            icon: "tablet"
         }])
     }, [])
-        
-        
     
     return (
         <View>
-            {cardList.map((card) => {
-                return <Card name={card.name} icon={card.icon} />
-            })}
+            <View style={styles.cardMenu}>
+                {cardList.map((card) => {
+                    return <Card name={card.name} icon={card.icon} />
+                })}
+            </View>
             
         </View>
     )
 }
 
-export default ButtonMenu
+const styles = StyleSheet.create({
+    cardMenu: {
+        //just for aesthetics, adjust as needed
+        top: 40,
+        left: 25,
+
+
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+
+        width: '90%',
+        height: '100%',
+
+    }
+})
+export default CardMenu
